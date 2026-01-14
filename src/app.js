9821f983a8c0879138d391a09996bc3bf4b7aa16
GauravTendulkar26 <gauravtendulkar1165@gmail.com>
2026-01-14 21:41:02 +0530

    set up backend environment

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import cookieParser from "cookieParser";
+
+const app = express();
+
+// use method design for middleware and configartion  
+app.use()
+
+
+
+export { app }
